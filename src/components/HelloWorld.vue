6fc2b8574f9368669cffb06dad5760eea7303252
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-07-08 12:56:55 +0900

    test button added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,100 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>API 호출후 data적재</h1>
+    <h1>마지막호출일 : {{ lastMigratedData }}</h1>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
+        <button v-on:click="onTest">API 호출 TEST</button>
       </li>
       <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
+        <button v-on:click="onMigrateData">적재</button>
       </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+      <h1>상태 : {{ message }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data: function () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      lastMigratedData: '없음',
+      message: ''
+    }
+  },
+  methods: {
+    onTest: function () {
+      this.$axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+        console.log('### response: ' + JSON.stringify(response))
+        this.message = response.data
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    onMigrateData: function () {
+      this.$axios.get('https://localhost:7030/WeatherForecast').then(response => {
+        console.log('### response: ' + JSON.stringify(response))
+        this.message = response.data
+      }).catch(error => {
+        console.log(error)
+      })
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
